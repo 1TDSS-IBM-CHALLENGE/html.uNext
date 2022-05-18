c9fb85b045ac54a8354dea8a1f07dd0927b3ff49
Gustavo Balero Cosse de Sousa <gubalero@hotmail.com>
2022-05-18 05:59:02 -0300

    Ajuste no slider da home

--- js/slider.js
+++ js/slider.js
@@ -1,41 +1,81 @@
 var $index = 0;
-var $totalSlides = 0;
-var $slides = [];
-var $indexSlider = []
+var $slides = document.querySelectorAll('.slide');
+var $indexSlider = document.querySelectorAll('.index-slider .fi');
+var $totalSlides = $indexSlider.length;
 var $posicaoClickStart = 0;
 var $posicaoClickEnd= 0;
 
-$(document).ready(
+// $(document).ready(
 
-    initSlider,
+//     initSlider,
 
-    $("div.icon").on('click', (e) => {
+//     $("div.icon").on('click', (e) => {
         
-        var botao = $(e.target)
+//         var botao = $(e.target)
 
-        if(botao.hasClass('direita') || botao.hasClass('fi-line-angle-right')){
-            toDireita()
-        }else{
-            toEsquerda()
-        }
-    }),
+//         if(botao.hasClass('direita') || botao.hasClass('fi-line-angle-right')){
+//             toDireita()
+//         }else{
+//             toEsquerda()
+//         }
+//     }),
 
-    $('.index-slider .fi').on('click', (e) => {
-        $index = $(e.target).data('index');
+//     $('.index-slider .fi').on('click', (e) => {
+//         $index = $(e.target).data('index');
+//         alteraSlide()
+//     }),
+
+//     $(".slide span").on('touchstart', (event) => {
+//         $posicaoClickStart = event.touches[0].clientX
+//     }),
+
+//     $(".slide span").on('touchmove', (event) => {
+//         event.preventDefault()
+//         $posicaoClickEnd = event.touches[0].clientX
+//     }),
+
+//     $(".slide span").on('touchend', () => {
+
+//         var totalDeslize = $posicaoClickStart - $posicaoClickEnd;
+        
+//         if( totalDeslize > 80 || totalDeslize < -80){
+//             if($posicaoClickStart < $posicaoClickEnd){
+//                 toEsquerda()
+//             }else{
+//                 toDireita()
+//             }
+//         }
+//     })
+    
+// );
+
+document.querySelector('.direita').addEventListener('click', () => {
+    toDireita()
+})
+
+document.querySelector('.esquerda').addEventListener('click', () => {
+    toEsquerda()
+})
+
+document.querySelectorAll('.index-slider .fi').forEach( (element) => {
+    element.addEventListener('click', () => {
+        $index = parseInt(element.attributes.getNamedItem('data-index').value)
         alteraSlide()
-    }),
+    })
+})
 
-    $(".slide span").on('touchstart', (event) => {
+document.querySelectorAll('.slide div').forEach((element) => {
+    
+    element.addEventListener('touchstart', (event) => {
         $posicaoClickStart = event.touches[0].clientX
-    }),
+    })
 
-    $(".slide span").on('touchmove', (event) => {
+    element.addEventListener('touchmove', (event) => {
         event.preventDefault()
         $posicaoClickEnd = event.touches[0].clientX
-    }),
+     })
 
-    $(".slide span").on('touchend', () => {
-
+     element.addEventListener('touchend', (event) => {
         var totalDeslize = $posicaoClickStart - $posicaoClickEnd;
         
         if( totalDeslize > 80 || totalDeslize < -80){
@@ -45,18 +85,10 @@ $(document).ready(
                 toDireita()
             }
         }
-    })
-    
-);
+     })
+})
 
 function initSlider(){
-    var slides = $('.slide');
-    var indexSlider = $('.index-slider .fi')
-    
-    $slides = slides
-    $indexSlider = indexSlider
-    $totalSlides = slides.length
-
     alteraSlide()
     trocaAutomatico()
 }
@@ -82,15 +114,15 @@ function toEsquerda(){
 
 function alteraSlide(){
  
-    $slides.each((index) => {
-        $($slides[index]).removeClass('selecionado')
-        $($indexSlider[index]).removeClass('fi-circle')
-        $($indexSlider[index]).addClass('fi-circle-line')
+    $slides.forEach((element, index) => {
+        $slides[index].classList.remove('selecionado')
+        $indexSlider[index].classList.remove('fi-circle')
+        $indexSlider[index].classList.add('fi-circle-line')
     });
 
-    $($slides[$index]).addClass('selecionado')
-    $($indexSlider[$index]).removeClass('fi-circle-line')
-    $($indexSlider[$index]).addClass('fi-circle')
+    $slides[$index].classList.add('selecionado')
+    $indexSlider[$index].classList.remove('fi-circle-line')
+    $indexSlider[$index].classList.add('fi-circle')
 }
 
 function trocaAutomatico(){
@@ -98,3 +130,5 @@ function trocaAutomatico(){
         toDireita()
     }, 8000)
 }
+
+initSlider()
